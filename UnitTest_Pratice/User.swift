6f5a8fa59b85b9209c6e2da9b53359e1ebfaa5da
500beckwon <kjsx60@gmail.com>
2022-09-24 22:25:25 +0900

    Add New Test Entities File

---
+++ UnitTest_Pratice/User.swift
@@ -0,0 +1,14 @@
+//
+//  User.swift
+//  UnitTest_Pratice
+//
+//  Created by ByungHoon Ann on 2022/09/24.
+//
+
+import Foundation
+
+struct User {
+    let email: String
+    let password: String
+
+}
