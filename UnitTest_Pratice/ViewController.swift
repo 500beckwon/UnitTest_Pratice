cd67303469ecaaae11aa1c9448f701c2add09a55
500beckwon <kjsx60@gmail.com>
2022-09-24 22:08:41 +0900

    Initial Commit

---
+++ UnitTest_Pratice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UnitTest_Pratice
+//
+//  Created by ByungHoon Ann on 2022/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
